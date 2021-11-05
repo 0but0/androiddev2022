6ea06a8811e0ca769f213cd1ccfbe4847c84f44d
0but0 <butbunbut@gmail.com>
2021-11-05 19:01:08 +0700

    Labwork 14

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.usthweather;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
@@ -70,8 +72,8 @@ public class WeatherActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.refresh:
-                Toast.makeText(this, "Refreshing...", Toast.LENGTH_SHORT).show();
-                super.onRestart();
+//                Toast.makeText(this, "Refreshing...", Toast.LENGTH_SHORT).show();
+//                super.onRestart();
                 Refresh();
                 return true;
             case R.id.settings:
@@ -81,31 +83,66 @@ public class WeatherActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @SuppressLint("StaticFieldLeak")
     public void Refresh(){
-        final Handler handler = new Handler(Looper.getMainLooper()){
+//        final Handler handler = new Handler(Looper.getMainLooper()){
+//            @Override
+//            public void handleMessage(@NonNull Message msg) {
+//                String content = msg.getData().getString("Server Response");
+//                Toast toast = Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        };
+//        Thread t = new Thread(()->{
+//            try{
+//                for (int i=0; i<5; i++){
+//                    Thread.sleep(1000);
+//                }
+//            } catch(InterruptedException e){
+//                e.printStackTrace();
+//            }
+//            Bundle bundle = new Bundle();
+//            bundle.putString("Server Response","New content is ready now");
+//            Message msg = new Message();
+//            msg.setData(bundle);
+//            handler.sendMessage(msg);
+//        });
+//        t.start();
+        AsyncTask<Integer, Integer, Integer> taskA = new AsyncTask<Integer, Integer, Integer>() {
             @Override
-            public void handleMessage(@NonNull Message msg) {
-                String content = msg.getData().getString("Server Response");
-                Toast toast = Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT);
+            protected void onPreExecute() {
+                Toast toast = Toast.makeText(getApplicationContext(), "Start to delay the app", Toast.LENGTH_SHORT);
                 toast.show();
             }
-        };
-        Thread t = new Thread(()->{
-            try{
-                for (int i=0; i<5; i++){
-                    Thread.sleep(1000);
-                }
-            } catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            Bundle bundle = new Bundle();
-            bundle.putString("Server Response","New content is ready now");
-            Message msg = new Message();
-            msg.setData(bundle);
-            handler.sendMessage(msg);
-        });
-        t.start();
 
+            @Override
+            protected void onPostExecute(Integer integer) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Finish delaying the app", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Updating "+values[0], Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @SuppressLint("StaticFieldLeak")
+            @Override
+            protected Integer doInBackground(Integer... integers) {
+                try{
+                    for (int i=0; i<5; i++){
+                        Thread.sleep(1000);
+                        publishProgress(i);
+                    }
+                } catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                return 0;
+        }
+    };
+    taskA.execute();
     }
 
     @Override
